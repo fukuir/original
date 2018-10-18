93a26aca0d3663b9fb7416c316c34fc50faf81ec
FukuiRyoutarou <calicocatff37@icloud.com>
2018-10-18 16:21:43 +0900

    初期段階

---
+++ Assets/Ray.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ray : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            //メインカメラ上のマウスカーソルのある位置からRayを飛ばす
+            UnityEngine.Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //レイヤーマスク作成
+            //int layerMask = LayerMaskNo.DEFAULT;
+
+            //Rayの長さ
+            float maxDistance = 10;
+
+            // RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, maxDistance, layerMask);
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, maxDistance);
+            //クリックしたオブジェクトを削除する
+            if (hit.collider)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/RefCon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefCon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right*10);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
